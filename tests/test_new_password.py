a70051490c989413f4a9ca89fa707fbd0eaa64e5
ahmetcinar06 <sehercancinar@gmail.com>
2026-02-11 21:39:50 +0300

    Yeni fonksiyonlar

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,36 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+def test_password_length():
+    length = 100
+    password = generate_password(length)
+    assert len(password) == length, "Oluşturulan şifre belirtilen uzunlukta değil."
+
+def test_password_uniqueness():
+    password1 = generate_password(100)
+    password2 = generate_password(100)
+    assert password1 != password2, "Şifreler aynı."
+
+def test_password_num():
+    password = generate_password(100)
+    found = False
+    i = 0
+    while not found:
+        if password[i].isdigit():
+            found = True
+        i = i + 1
+    assert found == True, "Şifre sayı içermiyor."
+
+def test_password_sign():
+    password = generate_password(100)
+    found = False
+    i = 0
+    while not found:
+        if password[i] in "+-*/?=(&%^'!><@|`.,)":
+            found = True
+        i = i + 1
+    assert found == True, "Şifre işaret içermiyor."
+
 """
 Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif olarak, kendi testinizi de oluşturabilirsiniz!
 Daha fazla test yazabilirseniz harika olur!
