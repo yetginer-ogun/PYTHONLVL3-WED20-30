c4fdcd9636c90c251e9dd7d7e18c77723fc0cd74
yetginer-ogun <yetginer.mehmetogun@gmail.com>
2026-02-11 21:39:41 +0300

    test eklendi

--- TUR-Python-LVL3-M7L3-sifre-uretici-main/tests/test_new_password.py
+++ TUR-Python-LVL3-M7L3-sifre-uretici-main/tests/test_new_password.py
@@ -15,3 +15,36 @@ Daha fazla test yazabilirseniz harika olur!
 1. Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin  
 2. Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test edin 
 """
+
+
+def test_password_length():
+    length = 50
+    password = generate_password(length)
+    assert len(password) == length, "Parola uzunluğu hatalı"
+
+def test_password_double():
+    password1 = generate_password(100)
+    password2 = generate_password(100)
+    assert password1 != password2, "Parolalar birbirinin aynısı"
+
+def test_password_contains_number():
+    password1 = generate_password(1000)
+    found = False
+    i = 0
+    while not found:
+        if password1[i].isdigit():
+            found = True
+        i = i + 1
+    assert found == True, "Şifre sayı içermiyor"
+
+def test_password_contains_sign():
+    password1 = generate_password(1000)
+    found = False
+    i = 0
+    while not found:
+        if password1[i] in "+-*/?-_=()<>|[]&%$#":
+            found = True
+        i = i + 1
+    assert found == True, "Şifre işaret içermiyor"
+
+
